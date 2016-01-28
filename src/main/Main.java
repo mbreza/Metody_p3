b190266659434c4f869b8d82731f1b532b717c7a
student <breza.mateusz@gmail.com>
2016-01-28 20:52:00 +0100

    counting error

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	static double[] outcome = new double[12];
 	
+	static double[] error = new double[12];
+	
 	static double[] outcome1 = new double[12];
 
 	public static void main(String[] args) {
@@ -26,21 +28,30 @@ public class Main {
 			radians = Math.toRadians(x);
 			
 
-			outcome[i] = outcome[i - 1] + h * (4 * Math.sin(radians) * Math.cos(radians));
-						
+			outcome[i] = outcome[i - 1] + h * (4 * Math.sin(radians) * Math.cos(radians));			
+			
 			x=(x+1) *h;
 
-			System.out.println("Euler:    "+outcome[i]);
+			System.out.println("Euler"+i+"   :"+outcome[i]);
 			
 		}
 
+		x=0;
+		
+		for (int i = 0 ; i<=10 ; i++){
+			
+			radians = Math.toRadians(x);
+			error[i] = outcome[i] - (2*Math.sin(radians)*Math.sin(radians)*Math.tan(radians));
+			System.out.println("Euler blad"+i+"   :"+error[i]);
+			x=(x+1) *h;
+		}
 	}
 	
 	
 	public static void Heun(int b, int N) {
 		
 		int h = b/N;
-		outcome[0] = 1;
+		outcome1[0] = 1;
 		double x = 0;
 		double fp;
 		double radians;
@@ -49,17 +60,26 @@ public class Main {
 
 			radians = Math.toRadians(x);								
 			
-			fp = Math.toRadians(outcome[i]);
+			fp = Math.toRadians(x+1);
 			
 			outcome1[i] = outcome1[i - 1] + h*((4 * Math.sin(radians) * Math.cos(radians))+(4 * Math.sin(fp) * Math.cos(fp)))/2;
 			
 			
 			x=(x+1) *h;
 			
-			System.out.println("Heun:    "+outcome1[i]);
+			System.out.println("Heun: "+i+"   :"+outcome1[i]);
 			
 		}
 
+		for (int i = 0 ; i<=10 ; i++){
+			
+			radians = Math.toRadians(x);
+			error[i] = outcome1[i] - (2*Math.sin(radians)*Math.sin(radians)*Math.tan(radians));
+			System.out.println("Heun blad"+i+"   :"+error[i]);
+			x=(x+1) *h;
+		}
+		
+		
 	}
 	
 	
